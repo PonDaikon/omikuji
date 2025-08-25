90b70e26bc37c4635ed4b3331415a429b5418a82
DESKTOP-UJ0KDV2\ronom <r.onomitsu@gmail.com>
2025-08-25 13:52:43 +0900

    ignoreリスト追加まで

--- omikuji/omikuji/Program.cs
+++ omikuji/omikuji/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿//おみくじ
+string[] omikuji = new string[5];
+omikuji[0] = "0大吉";
+omikuji[1] = "1中吉";
+omikuji[2] = "2吉";
+omikuji[3] = "3凶";
+omikuji[4] = "4大凶";
+
+//string[] omikuji = { "0大吉", "1中吉", "2吉", "3凶","4大凶" };
+
+//各おみくじを何回引いたかを格納する配列
+int[] count = new int[5];
+
+//Randomクラスを実体化して変数randが機能を参照できる
+Random rand = new Random();
+int result = rand.Next(0, 5);//0以上5未満のランダムを取得して変数resultに代入
+
+Console.WriteLine(omikuji[result]);
--- omikuji/omikuji/obj/Debug/net8.0/omikuji.AssemblyInfo.cs
+++ omikuji/omikuji/obj/Debug/net8.0/omikuji.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("omikuji")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58cd6f18c2e22a1755b6543dc5bd5136e4f7da75")]
 [assembly: System.Reflection.AssemblyProductAttribute("omikuji")]
 [assembly: System.Reflection.AssemblyTitleAttribute("omikuji")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
